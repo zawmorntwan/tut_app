3bfdafd9979371ec959560cf93b65bb932780154
Zaw Min Tun <zawmintun.tp@gmail.com>
2022-07-05 16:12:33 +0630

    update onboarding screen assets

--- lib/presentation/resources/assets_manager.dart
+++ lib/presentation/resources/assets_manager.dart
@@ -2,4 +2,8 @@ const String imagePath= 'assets/images';
 
 class ImageAssets{
   static const String splashLogo = '$imagePath/splash_logo.png';
+  static const String onboardingLogo1 = '$imagePath/onboarding_logo1.svg';
+  static const String onboardingLogo2 = '$imagePath/onboarding_logo2.svg';
+  static const String onboardingLogo3 = '$imagePath/onboarding_logo3.svg';
+  static const String onboardingLogo4 = '$imagePath/onboarding_logo4.svg';
 }
