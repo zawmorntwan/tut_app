494c3c2229399c254a7159532af5f909c6f1fc6e
Zaw Min Tun <zawmintun.tp@gmail.com>
2022-07-05 15:31:42 +0630

    update app icon and name

--- lib/presentation/main/main_view.dart
+++ lib/presentation/main/main_view.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
 class MainView extends StatefulWidget {
   const MainView({Key? key}) : super(key: key);
@@ -14,4 +12,4 @@ class _MainViewState extends State<MainView> {
   Widget build(BuildContext context) {
     return Scaffold();
   }
-}
+} 
